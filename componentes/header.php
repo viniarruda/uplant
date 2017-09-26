09fa50869b484ba0a727aa9bd598035b3a1c5b74
Vinícius Arruda de Souza <rm71275@fiap.com.br>
2017-09-26 01:27:43 -0300

    Adicionando script para cache

--- componentes/header.php
+++ componentes/header.php
@@ -41,6 +41,14 @@
 
     <link rel="stylesheet" href="//code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
     <link rel="manifest" href="/manifest.json"/>
+    <?php 
+        $g = gmdate('D, d M Y H:i:s');
+        header('Expires: ' . $g . ' GMT');
+        header('Last-Modified: ' . $g . ' GMT');
+        header('Cache-Control: no-store, no-cache, must-revalidate');
+        header('Cache-Control: post-check=0, pre-check=0', false);
+        header('Pragma: no-cache');
+     ?>
 </head>
 <body>
 <div id="wrapper">
