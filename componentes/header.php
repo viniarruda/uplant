abee8c32b1d145437a5ccbe703b9c1c84cc9e28d
Vinícius Arruda <viniarruda@HDU16053.local>
2017-09-26 09:33:58 -0300

    removing meta

--- componentes/header.php
+++ componentes/header.php
@@ -12,15 +12,6 @@
     <meta name="msapplication-TileImage" content="content/images/48d2b70d-eb8f-237e-c1c2-cd91e8facbcc.webPlatform.png">
     <meta name="msapplication-TileColor" content="#9ec25e">
     <meta name="theme-color" content="#9ec25e">
-    <!-- face meta -->
-    <meta property="og:url" content="https://www.uplant.com.br">
-    <meta property="og:title" content="Investimento agrículo crowdfunding">
-    <meta property="og:site_name" content="Uplant">
-    <meta property="og:description" content="Se você é uma Pessoa Física e quer investir de modo seguro, rápido e prático, esse é o lugar ideal para você.">
-    <meta property="og:image" content="content/images/image-share.png">
-    <meta property="og:image:type" content="image/png">
-    <meta property="og:image:width" content="500"> 
-    <meta property="og:image:height" content="171">
     <link href='https://fonts.googleapis.com/css?family=Open+Sans:400,600,700,300' rel='stylesheet' type='text/css'>
     <link rel="stylesheet" href="css/normalize.css"/>
     <link rel="stylesheet" href="css/jquery.sidr.light.css"/>
